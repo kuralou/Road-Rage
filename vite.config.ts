b2929fb64153d1171f8ace0faa7e34f4b7ade86e
kuralou <itsconvallaria@gmail.com>
2026-04-17 09:20:33 -0400

    mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  // Adding the base property ensures the site looks for files 
+  // in the /Road-Rage/ folder instead of the root.
+  base: './', 
   server: {
     port: 8000,
-    host: true // This allows you to test on your mobile phone over Wi-Fi
+    host: true 
   },
   build: {
     assetsInlineLimit: 0
